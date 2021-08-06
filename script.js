f1c28efe1357dcaef8e77eda4bf75193d49f9ac4
srinibas <srinibaskhuntia39@gmail.com>
2021-08-06 22:33:43 +0530

    Tanslator implemented

---
+++ script.js
@@ -0,0 +1,30 @@
+let translateButton = document.querySelector(".btn-translate");
+let inputText = document.querySelector("#textArea");
+let translatedArea = document.querySelector(".translatedArea");
+
+translateButton.addEventListener("click", function (e) {
+    let inputTextValue = inputText.value;
+    console.log(inputTextValue);
+    serverURL(inputTextValue);
+})
+
+function serverURL(inputTextValue) {
+    let URL = "https://api.funtranslations.com/translate/dothraki.json" + "?" + "text=" +inputTextValue;
+    APIcall(encodeURI(URL));
+}
+function APIcall(encodedURL) {
+
+    fetch(encodedURL)
+        .then(response => response.json())
+        .then(json => {
+
+            translatedArea.innerText = json.contents.translated;
+        }
+
+
+        )
+        .catch(function (error) {
+            console.log(error);
+            alert("Some error occured in the server please try after sometime")
+        })
+}
